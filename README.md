cb0c960d6299b552cb5caa41baaf43dbb9d79599
Matt Kruse <github@mattkruse.com>
2015-05-13 13:12:29 -0500

    Documentation, code tweaks

--- README.md
+++ README.md
@@ -1,18 +1,122 @@
-Alexa-App
-=========
+# alexa-app
 
 A Node module to simplify development of Alexa apps using Node.js
 
-## Installation
+# Installation
 
+```bash
 	npm install alexa-app --save
+```
 
-## Usage
+# Example App
 
-	var alexa = require('alexa-app');
-	var app = new alexa.app('sample');
-	app.intent('sample',function(request,response) {
-		var number = request.slot('number');
-		response.say("You asked for the number "+number);
-		response.shouldEndSession(true);
-	});
+The *[alexa-app-server](https://github.com/matt-kruse/alexa-app-server/)* project is a fully working example of using alexa-app to build an Alexa application using Node.js.
+
+# Summary
+
+alexa-app does the dirty work of interpretting the JSON request from Amazon and building the JSON response. It provides convenience methods to more easily build the response, handle session objects, and add cards. It also makes it easy to run multiple endpoints (apps) on one Node.js server instance.
+
+# Usage
+
+```javascript
+var alexa = require('alexa-app');
+var app = new alexa.app('sample');
+
+app.intent('number',function(request,response) {
+  var number = request.slot('number');
+  response.say("You asked for the number "+number);
+});
+
+express.post('/sample',app.request);
+```
+
+# Examples
+
+## LaunchRequest
+
+```javascript
+app.launch(function(request,response) {
+  response.say("Hello World");
+  response.card("Hello World","This is an example card");
+});
+```
+
+## IntentRequest
+
+Define the handler for multiple intents using multiple calls to intent().
+
+```javascript
+app.intent('buy', function(request,response) {
+	response.say("You bought a "+request.slot("item"));
+});
+app.intent('sell', function(request,response) {
+	response.say("You sold your items!");
+});
+```
+
+## SessionEndRequest
+
+```javascript
+app.sessionEnded(function(request,response) {
+	logout( request.userId );
+	// No response necessary
+});
+```
+
+## Keep a session open
+
+By default the session is closed after each request.
+
+```javascript
+app.intent('guess', function(request,response) {
+	response.say("Guess again!");
+	response.shouldEndSession(false);
+});
+```
+
+## Read/write session data
+
+```javascript
+app.launch(function(request,response) {
+	response.session('number',42);
+	response.say("Would you like to know the number?");
+	response.shouldEndSession(false);
+});
+app.intent('tellme', function(request,response) {
+	response.say("The number is "+request.session('number'));
+});
+```
+
+## Access the raw http request/response
+
+```javascript
+app.launch(function(request,response,http_req,http_res) {
+	log( http_req.ip );
+});
+```
+
+## Attach multiple apps to express
+
+```javascript
+var server = express();
+var alexa = require('alexa-app');
+
+var app1 = new alexa.app('hello');
+app1.launch(function(request,response) {
+  response.say("Hello");
+});
+
+var app2 = new alexa.app('world');
+app2.launch(function(request,response) {
+  response.say("World");
+});
+
+// Prefix all apps with /alexa/ => ex: /alexa/hello
+alexa.bootstrap(server,"/alexa/");
+```
+
+## Define a custom endpoint name for an app
+
+```javascript
+var app = new alexa.app('hello','myEndpointName');
+```
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// --------------------------------------------------------------------
-// ALEXA API
-// --------------------------------------------------------------------
 var alexa={};
+
 alexa.response = function() {
 	this.response = {
 		"version": "1.0",
@@ -19,7 +17,7 @@ alexa.response = function() {
 		}
 	};
 	this.card = function(title,content) {
-		this.response.response.card = {"type":"Simple","content":content};
+		this.response.response.card = {"type":"Simple","content":content,"title":title};
 	};
 	this.shouldEndSession = function(bool) {
 		this.response.response.shouldEndSession = bool;
@@ -28,6 +26,7 @@ alexa.response = function() {
 		this.response.sessionAttributes[key] = val;
 	};
 };
+
 alexa.request = function(json) {
 	this.data = json;
 	this.slot = function(slot_name,default_value) {
@@ -42,20 +41,26 @@ alexa.request = function(json) {
 		} catch(e) { }
 		return null;
 	};
-	this.session = {
+	this.sessionDetails = {
 		"new":this.data.session.new,
 		"sessionId":this.data.session.sessionId,
 		"userId":this.data.session.userId,
 		"attributes":this.data.session.attributes
 	};
-	this.sessionData = function(key) {
+	this.userId = this.data.session.userId;
+	this.sessionId = this.data.session.sessionId;
+	this.sessionAttributes = this.data.session.attributes;
+	this.isSessionNew = (true===this.data.session.new);
+	this.session = function(key) {
 		try {
 			return this.data.session.attributes[key];
 		} catch(e) { }
 		return;
 	};
 };
+
 alexa.apps = {};
+
 alexa.app = function(name,endpoint) {
 	var self = this;
 	this.endpoint = endpoint;
@@ -67,24 +72,31 @@ alexa.app = function(name,endpoint) {
 		}
 		this.intents[intentName] = {"function":func};
 		if (schema) {
-		
+			this.intents[intentName].schema = schema;
 		}
 	};
-	this.launch = null;
-	this.onLaunch = function(func) {
-		this.launch = func;
+	this.launchFunc = null;
+	this.launch = function(func) {
+		this.launchFunc = func;
 	};
-	this.sessionEnded = null;
-	this.onSessionEnded = function(func) {
-		this.sessionEnded = func;
+	this.sessionEndedFunc = null;
+	this.sessionEnded = function(func) {
+		this.sessionEndedFunc = func;
 	};
 	this.request = function(req,res) {
 		try {
+			var key;
 			var response = new alexa.response();
 			var request = new alexa.request(req.body);
-			
+			// Copy all the session attributes from the request into the response so they persist.
+			// This should happen by default, but it seems to be a bug in the Alexa API (?)
+			if (request.sessionAttributes) {
+				for (key in request.sessionAttributes) {
+					response.session(key, request.sessionAttributes[key]);
+				}
+			}
 			var requestType = request.type();
-			if ("IntentRequest"==requestType) {
+			if ("IntentRequest"===requestType) {
 				try {
 					var func = self.intents[req.body.request.intent.name]['function'];
 					if (typeof func=="function") {
@@ -95,17 +107,17 @@ alexa.app = function(name,endpoint) {
 					}
 				} catch(e) { }
 			}
-			else if ("LaunchRequest"==requestType) {
-				if (typeof self.launch=="function") {
-					self.launch(request,response);
+			else if ("LaunchRequest"===requestType) {
+				if (typeof self.launchFunc=="function") {
+					self.launchFunc(request,response,req,res);
 				}
 				else {
 					response.say("Try telling the application what to do instead of opening it");
 				}
 			}
-			else if ("SessionEndedRequest"==requestType) {
-				if (typeof self.sessionEnded=="function") {
-					self.sessionEnded(request,response);
+			else if ("SessionEndedRequest"===requestType) {
+				if (typeof self.sessionEndedFunc=="function") {
+					self.sessionEndedFunc(request,response,req,res);
 				}
 			}
 			else {
@@ -122,11 +134,14 @@ alexa.app = function(name,endpoint) {
 	};
 	alexa.apps[name] = this;
 }
-alexa.bootstrap = function(express) {
-	var app;
-	for (app in alexa.apps) {
-		express.post('/guessinggame',alexa.apps[app].request);
-		express.get('/guessinggame',alexa.apps[app].test);
+// A utility method to bootstrap alexa endpoints into express automatically
+alexa.bootstrap = function(express,path) {
+	var appName, app;
+	for (appName in alexa.apps) {
+		app = alexa.apps[appName];
+		var endpoint = (path || '/') + (app.endpoint || appName);
+		express.post(endpoint,app.request);
+		express.get(endpoint,app.test);
 	}
 };
 module.exports = alexa;
