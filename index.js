c1eedf30457a680898ff76fac9b33994156d6e55
Jason Priebe <jason.priebe@gmail.com>
2015-09-26 14:31:42 -0400

    added exhaustiveUtterances option to manage the size of the generated utterances list

--- index.js
+++ index.js
@@ -119,6 +119,9 @@ alexa.app = function(name,endpoint) {
 	
 	// Persist session variables from every request into every response?
 	this.persistentSession = true;
+
+	// use the minimal set of utterances or the full cartesian product?
+	this.exhaustiveUtterances = false;
 	
 	// A catch-all error handler - do nothing by default
 	this.error = null;
@@ -337,9 +340,73 @@ function expandShortcuts(str,slots,dictionary) {
   return [str];
 }
 
+var slotIndexes = [];
+function expandSlotValues (variations, slotSampleValues) {
+  var i;
+
+  var slot;
+  for (slot in slotSampleValues) {
+
+    var sampleValues = slotSampleValues[slot];
+
+    var idx = -1;
+    if (typeof slotIndexes[slot] !== "undefined") {
+      idx = slotIndexes[slot];
+    }
+
+    var newVariations = [];
+
+    // make sure we have enough variations that we can get through the sample values
+    // at least once for each alexa-app utterance...  this isn't strictly as 
+    // minimalistic as it could be.
+    //
+    // our *real* objective is to make sure that each sampleValue gets used once per
+    // intent, but each intent spans multiple utterances; it would require heavy
+    // restructuring of the way the utterances are constructed to keep track of
+    // whether every slot was given each sample value once within an Intent's set 
+    // of utterances.  So we take the easier route, which generates more utterances
+    // in the output (but still many less than we would get if we did the full 
+    // cartesian product). 
+    if (variations.length < sampleValues.length) {
+      var mod = variations.length;
+      var xtraidx = 0;
+      while (variations.length < sampleValues.length) {
+        variations.push (variations[xtraidx]);
+        xtraidx = (xtraidx + 1) % mod;
+      }
+    }
+
+    variations.forEach (function (variation, j) {
+      var newVariation = [];
+      //var logmsg = "[ ";
+      variation.forEach (function (value, k) {
+        if (value == "slot-" + slot) {
+          idx = (idx + 1) % sampleValues.length;
+          slotIndexes[slot] = idx;
+
+          //logmsg += "<" + value + " --> " + sampleValues[idx] + "> ";
+          value = sampleValues[idx];
+        }
+        else {
+          //logmsg += "<" + value + "> ";
+        }
+
+        newVariation.push (value);
+      });
+      //logmsg += "]";
+      //console.log (logmsg);
+      newVariations.push (newVariation);
+    });
+
+    variations = newVariations;
+  }
+
+  return variations;
+}
+
 // Generate a list of utterances from a template
-function generateUtterances(str,slots,dictionary) {
-  var placeholders=[], utterances=[], slotmap={};
+function generateUtterances(str,slots,dictionary,exhaustiveUtterances) {
+  var placeholders=[], utterances=[], slotmap={}, slotValues=[];
   // First extract sample placeholders values from the string
   str = str.replace(/\{([^\}]+)\}/g, function(match,p1) {
     var expandedValues=[], slot, values = p1.split("|");
@@ -353,12 +420,31 @@ function generateUtterances(str,slots,dictionary) {
     if (slot) {
       slotmap[slot] = placeholders.length;
     }
-    placeholders.push( expandedValues );
+
+    // if we're dealing with minimal utterances, we will delay the expansion of the
+    // values for the slots; all the non-slot expansions need to be fully expanded
+    // in the cartesian product
+    if (!exhaustiveUtterances && slot)
+    {
+      placeholders.push( [ "slot-" + slot ] );
+      slotValues[slot] = expandedValues;
+    }
+    else
+    {
+      placeholders.push( expandedValues );
+    }
+
     return "{"+(slot || placeholders.length-1)+"}";
   });
   // Generate all possible combinations using the cartesian product
   if (placeholders.length>0) {
     var variations = Combinatorics.cartesianProduct.apply(Combinatorics,placeholders).toArray();
+
+    if (!exhaustiveUtterances)
+    {
+      variations = expandSlotValues (variations, slotValues);
+    }
+
     // Substitute each combination back into the original string
     variations.forEach(function(values) {
       // Replace numeric placeholders
