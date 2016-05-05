9fe66e8345380a34796bff3e7dcd46465fb73337
josh skeen <josh@joshskeen.com>
2016-05-04 22:31:41 -0400

    adds clear test

--- test/test_alexa_app_intent_request.js
+++ test/test_alexa_app_intent_request.js
@@ -55,6 +55,43 @@ describe("Alexa", function() {
             app.intent("airportInfoIntent", {}, intentHandler);
 
             describe("outputSpeech", function() {
+              it("clears output when clear is called", function() {
+                app = new Alexa.app("myapp");
+                intentHandler = function(req, res) {
+                  res.say(expectedMessage).say(expectedMessage).clear();
+                  return true;
+                };
+                app.intent("airportInfoIntent", {}, intentHandler);
+                var subject = app.request(mockRequest);
+                subject = subject.then(function(response) {
+                  return response.response.outputSpeech;
+                });
+                return expect(subject).to.eventually.become({
+                  text: "",
+                  type: "PlainText"
+                });
+              });
+              it("clears output when clear is called and say is then called", function() {
+                app = new Alexa.app("myapp");
+                intentHandler = function(req, res) {
+                  res.say(expectedMessage)
+                    .say(expectedMessage)
+                    .clear().say(expectedMessage);
+                  return true;
+                };
+                app.intent("airportInfoIntent", {}, intentHandler);
+                var subject = app.request(mockRequest);
+                subject = subject.then(function(response) {
+                  return response.response.outputSpeech;
+                });
+                //this seems like a strange result - should type be ssml? looks like a bug
+                return expect(subject).to.eventually.become({
+                  ssml: "<speak>tubular!</speak>", 
+                  text: "",
+                  type: "PlainText"
+                });
+              });
+
               it("combines says into a larger response", function() {
                 app = new Alexa.app("myapp");
                 intentHandler = function(req, res) {
