b9c7175245d8de57180b37363ede426a2fe4bb0a
Bela Vizy <bela@opendog.org>
2017-01-17 17:13:09 -0500

    fixed test to 2 spaces ident

--- test/test_alexa_app_creation.js
+++ test/test_alexa_app_creation.js
@@ -13,14 +13,15 @@ describe("Alexa", function () {
     describe("instantiate app", function () {
       it("fails when alexa.app was called without the new keyword", function () {
         var message;
-          try {
-            var testApp = Alexa.app("testApp");
-          } catch (e) {
-            message = e.message;
-          }
 
-          return expect(Promise.resolve(message))
-            .to.eventually.equal("Function must be called with the new keyword");
+        try {
+          var testApp = Alexa.app("testApp");
+        } catch (e) {
+          message = e.message;
+        }
+
+        return expect(Promise.resolve(message))
+          .to.eventually.equal("Function must be called with the new keyword");
       });
     });
   });
