e69adf7ad7137eeea34bd0890af298c8dc6dd013
orly <orly.levf@gmail.com>
2018-10-21 14:28:21 +0300

    Update test/test_alexa_app_fulfill_intent_request.js

--- test/test_alexa_app_fulfill_intent_request.js
+++ test/test_alexa_app_fulfill_intent_request.js
@@ -18,17 +18,82 @@ describe("Alexa", function () {
         });
 
         describe("#CanFulfillIntent_request", function () {
+
             describe("response", function () {
                 var mockRequest = mockHelper.load("can_fulfill_intent_request_play_sound.json");
-                var request = testApp.request(mockRequest);
 
-                it("no handler responds with a exception", function() {                    
-          
-                    var subject = request.then(function(response) {
-                      return response.response;
+                var request = testApp.request(mockRequest);
+                beforeEach(function () {
+                    request = testApp.request(mockRequest);
+                });
+
+                it("no handler responds with a exception", function () {
+                    var subject = request.then(function (response) {
+                        return response.response;
                     });
                     return expect(subject).to.eventually.be.rejectedWith("NO_CAN_FULFILL_FUNCTION");
-                  });
+                });
+
+                it("empty handler responds with default NO for canFulfill", function () {
+                    testApp.pre = undefined;
+                    testApp.post = undefined;
+                    testApp.canFulfillIntent(function (req, res) {
+                    });
+
+                    var subject = request.then(function (response) {
+                        return response.response.canFulfillIntent.canFulfill;
+                    });
+                    return expect(subject).to.eventually.become('NO');
+                });
+
+                it("empty handler responds with default NO for slot Sound", function () {
+                    testApp.pre = undefined;
+                    testApp.post = undefined;
+                    testApp.canFulfillIntent(function (req, res) {
+                    });
+
+                    var subject = request.then(function (response) {
+                        return response.response.canFulfillIntent.slots;
+                    });
+                    return expect(subject).to.eventually.become({
+                        "Sound": {
+                            "canUnderstand": "NO",
+                            "canFulfill": "NO"
+                        }
+                    });
+                });
+
+                it("custom handler responds with default YES for canFulfill", function () {
+                    testApp.pre = undefined;
+                    testApp.post = undefined;
+                    testApp.canFulfillIntent(function (req, res) {
+                        res.canFulfill("YES");
+                    });
+
+                    var subject = request.then(function (response) {
+                        return response.response.canFulfillIntent.canFulfill;
+                    });
+                    return expect(subject).to.eventually.become('YES');
+                });
+
+                it("custom handler responds with default YES for slot Sound", function () {
+                    testApp.pre = undefined;
+                    testApp.post = undefined;
+                    testApp.canFulfillIntent(function (req, res) {
+                        res.canFulfillSlot("Sound","YES","YES");
+                    });
+
+                    var subject = request.then(function (response) {
+                        return response.response.canFulfillIntent.slots;
+                    });
+                    return expect(subject).to.eventually.become({
+                        "Sound": {
+                            "canUnderstand": "YES",
+                            "canFulfill": "YES"
+                        }
+                    });
+                });
+
             });
         });
     });
